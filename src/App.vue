092d7c4169eccf47021ede30219a1007c988aa20
Petar <petarsarac@hotmail.com>
2019-08-09 11:26:56 +0200

    first

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
     <div class="container-fluid">
-        <app-header></app-header>
-        
+        <app-header></app-header>    
             <transition name="slide" mode="out-in">
                 <router-view></router-view>
-            </transition>
-            
-        
-        
+            </transition>    
     </div>
 </template>
 
